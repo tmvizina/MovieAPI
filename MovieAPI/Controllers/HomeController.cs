a59ec08c569446f510d8adea55b7f9fbed0f9c64
tmvizina <tmvizina@gmail.com>
2021-07-04 19:20:55 -0400

    First Commit

---
+++ MovieAPI/Controllers/HomeController.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MovieAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using Newtonsoft.Json;
+
+
+namespace MovieAPI.Controllers
+{
+    public class HomeController : Controller
+    {
+        MovieDAL movieDAL = new MovieDAL();
+
+        public IActionResult Index()
+        {
+
+
+            return View();
+        }
+
+
+        public IActionResult MovieSearch()
+        {
+            return View();
+        }
+
+       [ HttpPost ]
+        public IActionResult MovieSearch(string title)
+        {
+            
+            
+           // string json =movieDAL.CallAPI(searchtype, title);
+
+            Movie m = movieDAL.GetMovie(title);
+
+            
+
+            return View(m);
+        }
+
+
+        public IActionResult MovieNight()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult MovieNight(string title1, string title2, string title3)
+        {
+
+            List<Movie> movienight = new List<Movie>();
+            // string json =movieDAL.CallAPI(searchtype, title);
+
+            Movie movie1 = movieDAL.GetMovieList(title1);
+            Movie movie2 = movieDAL.GetMovieList(title2);
+            Movie movie3 = movieDAL.GetMovieList(title3);
+
+            movienight.Add(movie1);
+            movienight.Add(movie2);
+            movienight.Add(movie3);
+
+            return View(movienight);
+        }
+
+
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MovieAPI/Models/MovieDAL.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace MovieAPI.Models
+{
+    public class MovieDAL
+    {
+        public string CallAPI(string searchType, string title)
+        {
+            
+            string key = "2133fdb8";
+            string url = @$"http://www.omdbapi.com/?{searchType}={title}&apikey={key}";
+            //This allows us to do either kind of search by choosing via the front end of the program.
+
+            HttpWebRequest request = WebRequest.CreateHttp(url);
+            //300 generally mean the server has moved or there has been a redirect
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            StreamReader rd = new StreamReader(response.GetResponseStream());
+
+            string json = rd.ReadToEnd();
+            rd.Close();
+            return json;
+
+        }
+    
+    public Movie GetMovie(string title)
+        {
+            string json =CallAPI("t", title);
+            Movie m =JsonConvert.DeserializeObject<Movie>(json);
+            return m;
+        }
+
+
+        public Movie GetMovieList(string title)
+        {
+            string json = CallAPI("t", title);
+            Movie m = JsonConvert.DeserializeObject<Movie>(json);
+            return m;
+        }
+
+    }
+}
